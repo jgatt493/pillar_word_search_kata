14b9944a98541d69f254c9d49bf24948a6fa0bc7
Gatt <jeremy.gatt@maxionwheels.com>
2018-03-07 13:52:46 -0500

    Added txt file, class init

---
+++ src/word_search.py
@@ -0,0 +1,10 @@
+class WordSearch:
+
+    """A class containing the necessary methods to solve a word search.
+    """
+
+    def __init__(self):
+        self.puzzle = open("puzzle.txt","r")
+
+
+this = WordSearch()
---
+++ src/word_search_test.py
@@ -0,0 +1,8 @@
+import unittest
+import word_search
+from word_search import WordSearch
+
+class WordSearchTests(unittest.TestCase):
+
+    def setUp(self):
+        self.word_search = WordSearch()
